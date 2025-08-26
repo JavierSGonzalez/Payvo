9e3e604444eaf156f17bd52abfcc37aabe088003
DaniloMCD <danilo.granados2026@hilasal.superate.org.sv>
2025-08-25 22:14:49 -0600

    Ingresos de info 1 2 y 3 terminados

---
+++ Ingreso de info/IndexInfoUno.js
@@ -0,0 +1,59 @@
+const btn = document.getElementById("btn")
+
+function seleccionButton(btn){
+
+    if(btn.classList.contains("seleccionado")){
+        btn.classList.remove("seleccionado");
+    } else{
+        btn.classList.add("seleccionado")
+    }
+   
+}
+
+const btn2 = document.getElementById("btn2")
+
+function seleccionButton2(btn2){
+
+    if(btn2.classList.contains("seleccionado2")){
+        btn2.classList.remove("seleccionado2");
+    } else{
+        btn2.classList.add("seleccionado2")
+    }
+   
+}
+
+const btn3 = document.getElementById("btn3")
+
+function seleccionButton3(btn3){
+
+    if(btn3.classList.contains("seleccionado3")){
+        btn3.classList.remove("seleccionado3");
+    } else{
+        btn3.classList.add("seleccionado3")
+    }
+   
+}
+
+const btn4 = document.getElementById("btn4")
+
+function seleccionButton4(btn4){
+
+    if(btn4.classList.contains("seleccionado4")){
+        btn4.classList.remove("seleccionado4");
+    } else{
+        btn4.classList.add("seleccionado4")
+    }
+   
+}
+
+const btn5 = document.getElementById("btn5")
+
+function seleccionButton5(btn5){
+
+    if(btn5.classList.contains("seleccionado5")){
+        btn5.classList.remove("seleccionado5");
+    } else{
+        btn5.classList.add("seleccionado5")
+    }
+   
+}
